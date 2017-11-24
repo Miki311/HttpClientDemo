2d6b44f6f83024053924761e803f1fc100b2adc4
Miodrag Simunovic <miodrag.simunovic@alite-international.com>
2017-11-24 16:39:38 +0100

    e

---
+++ HttpClient-1/AuthorisationTypes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace HttpClient_1
+{
+    public static class AuthorisationTypes
+    {
+        public enum HttpClientTypes
+        {
+            [Description("Unauthorized")]
+            Unauthorized,
+
+            [Description("AuthorizationToken")]
+            AuthorizationToken
+        };
+
+    }
+}
